19d202084eb4d1e56db1621e3ca74df45806ad79
albgje <46426339+albgje@users.noreply.github.com>
2022-06-07 16:02:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-ntime = '1w2d3h4m5s'
-wtime = '1day2weeks3s5hours10m'
-wwtime = '11m2w3d4s12h'
+ntime = "1w2d3h4m5s"
+wtime = "1day2weeks3s5hours10m"
+wwtime = "11m2w3d4s12h"
 
 import json
 
@@ -24,23 +24,23 @@ def parse_str(x):
             temp.append(c)
         else:
             break
-    return str(''.join(temp)) if ''.join(temp) else None
+    return str("".join(temp)) if "".join(temp) else None
 
 def num_join(string):
     if type(string) == list:
-        string = ''.join(string)
+        string = "".join(string)
     elif type(string) == tuple:
-        string = ''.join(string)
+        string = "".join(string)
     elif type(string) == int or type(string) == float:
         raise TypeError("You can only pass tuples lists or strings.")
     elif type(string) == str:
         string = string
     string = Format(string)
-    string = string.replace('w', ' w ')
-    string = string.replace('d', ' d ')
-    string = string.replace('h', ' h ')
-    string = string.replace('m', ' m ')
-    string = string.replace('s', ' s ')
+    string = string.replace("w", " w ")
+    string = string.replace("d", " d ")
+    string = string.replace("h", " h ")
+    string = string.replace("m", " m ")
+    string = string.replace("s", " s ")
     string = string.split()
     return string
     
@@ -55,43 +55,43 @@ def get_time(time:str):
         for  unit in range(len(ftimel)):
             try:
                 if ftimel[unit].isalpha():
-                    ftimel[unit] = '"' + ftimel[unit] + '"' + ':'
+                    ftimel[unit] = """ + ftimel[unit] + """ + ":"
                 elif ftimel[unit].isdigit() and ftimel[unit + 1].isalpha():
-                    ftimel[unit] = ftimel[unit] + ','
+                    ftimel[unit] = ftimel[unit] + ","
             except IndexError:
                 pass
             except Exception as e:
                 print(e)
 
-        ftimel[0] = '{' + ftimel[0]
-        ftimel[-1] = ftimel[-1] + '}'
+        ftimel[0] = "{" + ftimel[0]
+        ftimel[-1] = ftimel[-1] + "}"
 
-        ftimel = ''.join(ftimel)
+        ftimel = "".join(ftimel)
 
         res = json.loads(ftimel)
 
-        if 'w' not in res.keys():
-            res['w'] = 0
-        if 'd' not in res.keys():
-            res['d'] = 0
-        if 'h' not in res.keys():
-            res['h'] = 0
-        if 'm' not in res.keys():
-            res['m'] = 0
-        if 's' not in res.keys():
-            res['s'] = 0
+        if "w" not in res.keys():
+            res["w"] = 0
+        if "d" not in res.keys():
+            res["d"] = 0
+        if "h" not in res.keys():
+            res["h"] = 0
+        if "m" not in res.keys():
+            res["m"] = 0
+        if "s" not in res.keys():
+            res["s"] = 0
         #print(res, type(res))
         return res
 
     except:
-        print('Error')
-        rtrn = {'s':0,'w':0,'d':0,'h':0,'m':0}
+        print("Error")
+        rtrn = {"s":0,"w":0,"d":0,"h":0,"m":0}
         return rtrn
 
 
 def Format(unFormattedTimeString:str):
     ufts = unFormattedTimeString.lower()
-    uftsr = ufts.replace('weeks', 'w').replace('week', 'w').replace('days', 'd').replace('day', 'd').replace('hours', 'h').replace('hour', 'h').replace('minutes', 'm').replace('minute', 'm').replace('seconds', 's').replace('second','s')
+    uftsr = ufts.replace("weeks", "w").replace("week", "w").replace("days", "d").replace("day", "d").replace("hours", "h").replace("hour", "h").replace("minutes", "m").replace("minute", "m").replace("seconds", "s").replace("second","s")
 
     return uftsr
 
@@ -100,17 +100,17 @@ def Format(unFormattedTimeString:str):
 
 def get_time_to_seconds(time_str: str):
     time = get_time(Format(time_str))
-    seconds = time['s']
-    minutes = time['m'] * 60
-    hours = time['h'] * 60 * 60
-    days = time['d'] * 60 * 60 * 24
-    weeks = time['w'] * 60 * 60 * 24 * 7
+    seconds = time["s"]
+    minutes = time["m"] * 60
+    hours = time["h"] * 60 * 60
+    days = time["d"] * 60 * 60 * 24
+    weeks = time["w"] * 60 * 60 * 24 * 7
     timeInSeconds = seconds + minutes + hours + days + weeks
     #print(timeInSeconds, type(timeInSeconds))
     return timeInSeconds
 
 def get_seconds_to_str(seconds: int):
-    ''' From a number of seconds determines the amount of years, months, weeks, days, hours, minutes, and seconds. 
+    """ From a number of seconds determines the amount of years, months, weeks, days, hours, minutes, and seconds. 
 
     Assumes: 1 millenia = 10 centuries
              1 century = 10 decades
@@ -121,7 +121,7 @@ def get_seconds_to_str(seconds: int):
              1 day = 24 hours
              1 hour = 60 minutes
              1 minute = 60 seconds
-    '''
+    """
 
 
     minutes, seconds = divmod(seconds, 60)
@@ -135,107 +135,107 @@ def get_seconds_to_str(seconds: int):
     millenia,centuries=divmod(centuries, 10)
 
     if millenia == 0:
-        millenium = ''
+        millenium = ""
     elif millenia == 1:
-        millenium = '1 millenium'
+        millenium = "1 millenium"
     elif millenia >= 2:
-        millenium = '{} millenia'.format(millenia)
+        millenium = "{} millenia".format(millenia)
     if centuries == 0:
-        century = ''
+        century = ""
     elif centuries == 1:
-        century = '1 century'
+        century = "1 century"
     elif centuries >= 2:
-        century = '{} centuries'.format(centuries)
+        century = "{} centuries".format(centuries)
     if decades == 0:
-        decade = ''
+        decade = ""
     elif decades == 1:
-        decade = '1 decade'
+        decade = "1 decade"
     elif decades >= 2:
-        decade = '{} decades'.format(decades)
+        decade = "{} decades".format(decades)
     if years == 0:
-        year = ''
+        year = ""
     elif years == 1:
-        year = '1 year'
+        year = "1 year"
     elif years >= 2:
-        year = '{} years'.format(years)
+        year = "{} years".format(years)
     if months == 0:
-        month = ''
+        month = ""
     elif months == 1:
-        month = '1 month'
+        month = "1 month"
     elif months >= 2:
-        month = '{} months'.format(months)
+        month = "{} months".format(months)
     if weeks == 0:
-        week = ''
+        week = ""
     elif weeks == 1:
-        week = '1 week'
+        week = "1 week"
     elif weeks >= 2:
-        week = '{} weeks'.format(weeks)
+        week = "{} weeks".format(weeks)
     if days == 0:
-        day = ''
+        day = ""
     elif days == 1:
-        day = '1 day'
+        day = "1 day"
     elif days >= 2:
-        day = '{} days'.format(days)
+        day = "{} days".format(days)
     if hours == 0:
-        hour = ''
+        hour = ""
     elif hours == 1:
-        hour = '1 hour'
+        hour = "1 hour"
     elif hours >= 2:
-        hour = '{} hours'.format(hours)
+        hour = "{} hours".format(hours)
     if minutes == 0:
-        minute = ''
+        minute = ""
     elif minutes == 1:
-        minute = '1 minute'
+        minute = "1 minute"
     elif minutes >= 2:
-        minute = '{} minutes'.format(minutes)
+        minute = "{} minutes".format(minutes)
     if seconds == 0:
-        second = ''
+        second = ""
     elif seconds == 1:
-        second = '1 second'
+        second = "1 second"
     elif seconds >= 2:
-        second = '{} seconds'.format(seconds)
+        second = "{} seconds".format(seconds)
 
-    if millenium != '':
-        century = ' ' + century
-    if century != '':
-        decade = ' ' + decade
-    if decade != '':
-        year = ' ' + year
-    if year != '':
-        month = ' ' + month
-    if month != '':
-        week = ' ' + week
-    if week != '':
-        day = ' ' + day
-    if day != '':
-        hour = ' ' + hour
-    if hour != '':
-        minute = ' ' + minute
-    if minute != '':
-        second = ' ' + second
+    if millenium != "":
+        century = " " + century
+    if century != "":
+        decade = " " + decade
+    if decade != "":
+        year = " " + year
+    if year != "":
+        month = " " + month
+    if month != "":
+        week = " " + week
+    if week != "":
+        day = " " + day
+    if day != "":
+        hour = " " + hour
+    if hour != "":
+        minute = " " + minute
+    if minute != "":
+        second = " " + second
 
     formatted = millenium + century + decade + year + month + week + day + hour + minute + second
-    formatted = formatted.strip() + '.'
+    formatted = formatted.strip() + "."
     formaty = []
     formatyo = []
     formatyo = formatted.split()
     for i in formatyo:
-        if i != '':
+        if i != "":
             formaty.append(i)
-    return ' '.join(formaty)
+    return " ".join(formaty)
 
 
     
 
 def main():
-    print('\n\n')
+    print("\n\n")
     h = get_time_to_seconds(ntime)
     print(h)
     print()
     h = get_seconds_to_str(get_time_to_seconds(ntime))
     print(h)
     print()
-    print('-' * 10)
+    print("-" * 10)
     print()
     h = get_time_to_seconds(wtime)
     print(h)
@@ -243,15 +243,15 @@ def main():
     h = get_seconds_to_str(get_time_to_seconds(wtime))
     print(h)
     print()
-    print('-' * 10)
+    print("-" * 10)
     print()
     h = get_time_to_seconds(wwtime)
     print(h)
     print()
     h = get_seconds_to_str(get_time_to_seconds(wwtime))
     print(h)
-    print('\n\n')
+    print("\n\n")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
